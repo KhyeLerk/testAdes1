ae5f5e982a151f79bb5302ceb5509d265973935a
Bk49 <ericngyongwei@gmail.com>
2020-05-04 21:07:59 +0800

    Update Tab1.tsx

--- mobile/photo-gallery/src/pages/Tab1.tsx
+++ mobile/photo-gallery/src/pages/Tab1.tsx
@@ -8,11 +8,11 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Hello World</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
+      <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
