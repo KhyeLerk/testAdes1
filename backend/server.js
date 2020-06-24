8142ed9ae72a3ed9b954b0d88f63c38098faaae8
Bk49 <ericngyongwei@gmail.com>
2020-06-24 09:43:48 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,10 @@ Name : Eric Ng Yong Wei
 */
 var app = require('./controller/app.js');
 
-var port= 3000
+var port= process.env.PORT || 3000
 
 var server = app.listen(port, function () {
 
     console.log('Web App Hosted at http://localhost:%s', port);
 
 });
-
-// app.get("/login", (req, res) => {
-//     res.sendFile("login.html", { root: "./public/html-CA2" });
-//     console.log("Login page sent")
-// });
